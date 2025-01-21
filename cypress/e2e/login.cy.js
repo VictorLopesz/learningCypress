49b2e1627a09a76005c7eb8daa9d0ae85e85429b
Victor Lopes <euvictor_h@hotmail.com>
2025-01-20 23:17:09 -0300

    utilizando o commands para customizar testes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,74 +1,44 @@
-describe('login', () => {
-
+describe("login", () => {
   beforeEach(() => {
-    cy.viewport(1440, 900)
+    cy.viewport(1440, 900);
 
-    cy.visit('https://playground.cyskills.com.br/login')
-   
-    cy.contains('h2', 'Faça login')
-      .should('be.visible')
-  })
+    cy.visit("https://playground.cyskills.com.br/login");
 
-  it('Deve logar com sucesso', () => {
-    cy.get('[data-cy="email"]')
-      .type('papito@cyskills.com.br')
+    cy.contains("h2", "Faça login").should("be.visible");
+  });
 
-    cy.get('[data-cy="password"]')
-      .type('showtime')
-
-    cy.get('[data-cy="login-button"]')
-      .click()
+  it("Deve logar com sucesso", () => {
+    cy.login("papito@cyskills.com.br", "showtime");
 
     cy.get('[data-cy="welcome-title"]')
-      .should('be.visible')
-      .and('have.text', 'Boas vindas ao Cypress Playground')
-  })
+      .should("be.visible")
+      .and("have.text", "Boas vindas ao Cypress Playground");
+  });
 
-  it('Não deve logar com senha inválida', () => {
-    cy.get('[data-cy="email"]')
-      .type('papito@cyskills.com.br')
+  it("Não deve logar com senha inválida", () => {
+    cy.login("papito@cyskills.com.br", "123456");
+    cy.noticeHave("E-mail ou senha incorretos. Por favor, tente novamente!");
+  });
 
-    cy.get('[data-cy="password"]')
-      .type('121245')
+  it("Não deve logar com e-mail não cadastrado", () => {
+    cy.login("404@cyskills.com.br", "showtime");
+    cy.noticeHave("E-mail ou senha incorretos. Por favor, tente novamente!");
+  });
 
-    cy.get('[data-cy="login-button"]')
-      .click()
+  it("Não deve logar com e-mail incorreto", () => {
+    cy.login("www.linkedin.com", "showtime");
+    cy.noticeHave(
+      "O formato do e-mail está incorreto. Por favor, verifique e tente novamente!"
+    );
+  });
+});
 
-    cy.get('.notice p')
-      .should('be.visible')
-      .and('have.text', 'E-mail ou senha incorretos. Por favor, tente novamente!')
-  })
+Cypress.Commands.add("login", (email, password) => {
+  cy.get('[data-cy="email"]').type(email);
+  cy.get('[data-cy="password"]').type(password);
+  cy.get('[data-cy="login-button"]').click();
+});
 
-  it('Não deve logar com e-mail não cadastrado', () => {
-    cy.get('[data-cy="email"]')
-      .type('404@cyskills.com.br')
-
-    cy.get('[data-cy="password"]')
-      .type('showtime')
-
-    cy.get('[data-cy="login-button"]')
-      .click()
-
-    cy.get('.notice p')
-      .should('be.visible')
-      .and('have.text', 'E-mail ou senha incorretos. Por favor, tente novamente!')
-  })
-
-  
-  it('Não deve logar com e-mail incorreto', () => {
-    cy.get('[data-cy="email"]')
-      .type('www.linkedin.com')
-
-    cy.get('[data-cy="password"]')
-      .type('showtime')
-
-    cy.get('[data-cy="login-button"]')
-      .click()
-
-    cy.get('.notice p')
-      .should('be.visible')
-      .and('have.text', 'O formato do e-mail está incorreto. Por favor, verifique e tente novamente!')
-  })
-})
-
-
+Cypress.Commands.add("noticeHave", (text) => {
+  cy.get(".notice p").should("be.visible").and("have.text", text);
+});
