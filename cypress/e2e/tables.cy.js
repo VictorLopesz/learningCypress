a8f15f503be6b24d7aafa691bafbe7c9c0de07d2
Victor Lopes <euvictor_h@hotmail.com>
2025-07-18 22:32:15 -0300

    excluindo elemento da tabela

--- cypress/e2e/tables.cy.js
+++ cypress/e2e/tables.cy.js
@@ -18,11 +18,41 @@ describe('Tables', () => {
           .should('contain', 'papitodev')
     })
 
-    it.only('Deve verificar se está indisponível e inativo', () =>{
+    it('Deve verificar se está indisponível e inativo', () =>{
 
         cy.contains('table tbody tr', '1002')
             .should('be.visible')
             .should('contain', 'Facebook')
             .should('contain', 'Indisponível')
+    })
+
+    it('Deve remover uma rede social', () => {
+
+        const name = 'Facebook'
+
+        cy.contains('table tbody tr', '1002') // pegando o elemento pelo ID
+            .find('.remove-item') // depois de encontra-lo
+            .click() // utiliza o evento de clique
+
+        cy.contains('button', 'Excluir') 
+            .click()
+
+        cy.get('table tbody') // faz a validação para saber se o elemento foi excluído
+            .should('not.contain', name) // utiliza a constante para verificar se não contém mais ele
+    })
+
+     it('Deve permanecer na tabela ao desistir da exclusão', () => {
+
+        const name = 'Youtube'
+
+        cy.contains('table tbody tr', '1005') // pegando o elemento pelo ID
+            .find('.remove-item') // depois de encontra-lo
+            .click() // utiliza o evento de clique
+
+        cy.contains('button', 'Cancelar') 
+            .click()
+
+        cy.get('table tbody') // faz a validação para saber se o elemento NÃO foi excluído
+            .should('be.visible', name) // utiliza a constante para verificar se ele permanece lá
     })
 })
