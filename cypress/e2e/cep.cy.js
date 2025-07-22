49314350d32fd9f892bd0297ecd88b6d3047a24f
Victor Lopes <euvictor_h@hotmail.com>
2025-07-21 22:09:33 -0300

    Utilizando intercept para testar e manipular a aplicação

--- cypress/e2e/cep.cy.js
+++ cypress/e2e/cep.cy.js
@@ -6,16 +6,38 @@ describe('CEP', () => {
     })
 
     it('Deve cadastrar um endereço consumindo a API dos correios', () => {
-        cy.get('input[name="cep"]').type('25070260')
+      
+    //criado um objeto para organização e preenchimento dos campos requisitados        
+        const endereco = {
+            cep: 25070260,
+            logradouro: 'Rua itabira',
+            localidade: 'Duque de Caxias',
+            uf: 'RJ'
+        }
+        
+        //Exemplo de interceptação de serviço quando estiver fora do ar, forçando o statusCode de 200
+        //Utilizado para caso a API utilizada esteja fora do ar.
+        //Necessário utilizar a requisição 'GET', o link da API, abrir chaves e passar o Status 200.
+
+        cy.intercept('GET', `https://viacep.com.br/ws/${endereco.cep}/json/`, {
+            statusCode: 200,
+            //o objeto é passado dentro da interceptação
+            body: endereco
+        }).as('getCep')
+      
+        
+        cy.get('input[name="cep"]').type(endereco.cep)
         cy.contains('button', 'Cadastrar').click() //é utilizado o Contains devido ao texto do button, já que não tem name/id
    
+        cy.wait('@getCep') // utilizado para o cypress esperar a requisição ser executada com sucesso
+
         cy.get('input[name="logradouro"]', {timeout: 7000}) //timeout utilizado para o sistema buscar a informação no CEP
-            .should('have.value', 'Rua Itabira')
+            .should('have.value', endereco.logradouro)
 
         cy.get('input[name="cidade"]', {timeout: 7000}) //timeout utilizado para o sistema buscar a informação no CEP
-            .should('have.value', 'Duque de Caxias')
+            .should('have.value', endereco.localidade)
 
         cy.get('input[name="estado"]', {timeout: 7000}) //timeout utilizado para o sistema buscar a informação no CEP
-            .should('have.value', 'RJ')
+            .should('have.value', endereco.uf)
     })
 })
